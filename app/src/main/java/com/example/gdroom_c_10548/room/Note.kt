364f1faa8e66bc2d405fb4bc9c89b8eb604de9dd
philipferdinand <philipfs150302@gmail.com>
2022-09-21 20:25:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gdroom_c_10548/room/Note.kt
@@ -0,0 +1,14 @@
+package com.example.gdroom_c_10548.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+
+data class
+Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
